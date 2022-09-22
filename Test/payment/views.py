dc7c2c641fde5603f260290f394b2870e1b545f4
Margulan Zholaman <margulan.zholaman@gmail.com>
2022-09-22 09:32:57 +0600

    added order model

--- Test/payment/views.py
+++ Test/payment/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .models import Item
+from .models import Item, Order
 import stripe
 from django.http import HttpRequest, JsonResponse
+from django.db.models import Sum
 # Create your views here.
 
 
@@ -12,16 +13,19 @@ from django.http import HttpRequest, JsonResponse
 
 class BuyView(APIView):
 	def get(self, request, pk):
-		item = Item.objects.get(pk = pk)
+		order = Order.objects.get(pk = pk)
+		items = order.item.all()
+		total_price = items.aggregate(Sum('price'))
+		order_name = f"Order #{pk}"
 		stripe.api_key = 'sk_test_51LkUkGE0hznV006AvUNjVykKyx9HovhZb6rbnxEGcCat9xnvPJ5XIWx7bmMwbEchMh2lj5TsKRvYgR73pELC7h1H00oPJcSyzY'
 		session = stripe.checkout.Session.create(
 		    line_items=[{
 		      'price_data': {
 		        'currency': 'usd',
 		        'product_data': {
-		          'name': item.name,
+		          'name': order_name,
 		        },
-		        'unit_amount': item.price,
+		        'unit_amount':total_price['price__sum'],
 		      },
 		      'quantity': 1,
 		    }],
@@ -30,12 +34,10 @@ class BuyView(APIView):
 		    cancel_url='https://example.com/cancel',
 		  )
 		session_id = session.id
+
 		return JsonResponse({'sessionId': session['id']})
 
 def Items(request,pk):
-	item = Item.objects.get(pk = pk)
-	context = {'id':item.id,'product_name':item.name,'description':item.description,'price':item.price}
+	order = Order.objects.get(pk = pk)
+	context = {'id':order.id,'order':order.item.all()}
 	return render(request, 'payment/index.html',context)
-
-
-
